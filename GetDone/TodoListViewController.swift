4bf22187666561f3d8d74412d422d359cfcd3244
Julian Mclain <julianrmclain@gmail.com>
2018-11-12 09:34:42 -0800

    Completed add button functionality

--- GetDone/TodoListViewController.swift
+++ GetDone/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Take out the trash", "Clean up the house", "Laundry"]
+    var itemArray = ["Take out the trash", "Clean up the house", "Laundry"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +21,10 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
     
@@ -39,9 +37,29 @@ class TodoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //Mark - Add button functionality
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        // Local variable to store text field contents
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // Add text field contents to itemsArray and reload the table
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            // Set the text field local variable equal to what is in alertTextField. Must be done because alertTextField is only available in this closure which is executed when textField gets added to the Alert Controller
+            textField = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 
 }
 
